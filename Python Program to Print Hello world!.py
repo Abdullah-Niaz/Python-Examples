f5bc40b9a015f4c9a0189630cf67d483d8fcb4e1
Abdullah-Niaz <abdullahniaz423@gmail.com>
2022-11-10 00:45:52 +0500

    Example 1

---
+++ Python Program to Print Hello world!.py
@@ -0,0 +1,8 @@
+# A simple program that displays “Hello, World!”.
+# It's often used to illustrate the syntax of the language.
+# This program prints Hello, world!
+
+print('Hello, world!')
+
+# In this program, we have used the built-in print()
+# function to print the string Hello, world! on our screen.
