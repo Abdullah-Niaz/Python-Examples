da1c68c909f69cd98655f39b619160dc05c1b3a4
Abdullah-Niaz <abdullahniaz423@gmail.com>
2022-11-10 16:48:13 +0500

    3 Example

---
+++ 3 Python Program to Find the Square Root.py
@@ -0,0 +1,12 @@
+# In this program, you'll learn to find the square root of a number
+# using exponent operator and cmath module.
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of 8 is ', (num ,num_sqrt))
