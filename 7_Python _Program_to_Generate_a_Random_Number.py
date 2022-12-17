1c8661e67f3a29085387d4773aedf6e3ce1ebdd2
Abdulah-Niaz <abdullahniaz423@gmail.com>
2022-12-17 15:40:31 +0500

    7 Exmaple

---
+++ 7_Python _Program_to_Generate_a_Random_Number.py
@@ -0,0 +1,9 @@
+# In this example, you will learn to generate a random number in Python.
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+# Random integers
+print(random.randint(0, 9))
+# Random floating numbers
+print(random.uniform(0, 9))
