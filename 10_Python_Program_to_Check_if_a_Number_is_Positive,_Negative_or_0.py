f564758719346e08cac4f9ed93a8ad69ba22366d
Abdulah-Niaz <abdullahniaz423@gmail.com>
2022-12-17 17:47:00 +0500

    10 Example

--- 10_Python_Program_to_Check_if_a_Number_is_Positive,_Negative_or_0.py
+++ 10_Python_Program_to_Check_if_a_Number_is_Positive,_Negative_or_0.py
@@ -1,9 +1,9 @@
 # In this example, you will learn to check whether a number entered by the user is positive, negative or zero. This problem is solved using if...elif...else and nested if...else statement.
 
-num = int(input("Enter a number: "))
+num = int(input("Enter a Number: "))
 if num > 0:
-    print("Positive number")
+    print("Positive Number")
 elif num == 0:
     print("Zero")
 else:
-    print("Negative number")
+    print("Negative Number")
