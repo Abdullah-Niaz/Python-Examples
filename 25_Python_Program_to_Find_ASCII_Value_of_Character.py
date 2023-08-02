9397f715ca31d69d18a46f1535d6f74e9f9df697
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-02 22:57:43 +0500

    ASCII Values of Character

---
+++ 25_Python_Program_to_Find_ASCII_Value_of_Character.py
@@ -0,0 +1,10 @@
+# ASCII stands for American Standard Code for Information Interchange.
+#
+# It is a numeric value given to different characters and symbols, for computers to store and manipulate.
+# For example, the ASCII value of the letter 'A' is 65.
+
+# Program to find the ASCII value of the given character
+
+for i in range(0,255):
+    print("value of ", i , " is: ", chr(i))
+    print(end="\n")
